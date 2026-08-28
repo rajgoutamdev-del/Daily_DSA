2f816fc4727e9575f04df25f07ad73973320b2ba
rajgoutamdev-del <raj.goutam.dev@gmail.com>
2026-08-28 11:52:24 +0530

    Left view of binary tree solution in java

---
+++ Trees/left_view_binary_tree.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public void func(TreeNode root,int level, List<Integer> ans) {
+        if(root == null) return;
+        if(ans.size() < level) ans.add(root.val);
+        func(root.left,level+1,ans);
+        func(root.right,level+1,ans);
+
+    }
+    public List<Integer> leftSideView(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        func(root,1,ans);
+        return ans;
+
+    }
+}
